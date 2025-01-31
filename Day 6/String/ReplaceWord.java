c9b188956e8e70483ae09fc82192f9025f9e0cc3
AVIRALSINGH67 <aviral.raghuvansh1411@gmail.com>
2025-01-31 11:18:55 +0530

    Deleted old Files

--- Day 6/String/ReplaceWord.java
+++
@@ -1,8 +0,0 @@
-public class ReplaceWord {
-    public static void main(String[] args) {
-        String sentence = "Java is fun";
-        String wordToReplace = "Java";
-        String replacement = "Python";
-        System.out.println("Modified Sentence: " + sentence.replace(wordToReplace, replacement));
-    }
-}
